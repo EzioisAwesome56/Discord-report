c019eae470d8b2216e08f3e909bccb6fd6c3263f
EzioisAwesome56 <jedijon567@gmail.com>
2017-08-17 17:56:36 -0400

    fixed a bug

--- discordreport/editpost.php
+++ discordreport/editpost.php
@@ -11,9 +11,9 @@ $link = getServerDomainNoSlash().actionLink("post", $pid);
 
 ircReport("Post edited by "
 	.ircUserColor($thename, $loguser['sex'], 0)
-	."\003$c2: \003$c1"
+	.": "
 	.$thread["title"]
-	."\003$c2 (".$fpage.$forum["title"].")"
+	."(".$fpage.$forum["title"].")"
 	." -- "
 	.$link
 	);
--- discordreport/newreply.php
+++ discordreport/newreply.php
@@ -12,11 +12,11 @@ if($loguser["displayname"])
 $fpage = ircForumPrefix($forum);
 $link = getServerDomainNoSlash().actionLink("post", $pid);
 
-ircReport("\003".$c2."New reply by\003$c1 "
+ircReport("\003".$c2."New reply by "
 	.ircUserColor($thename, $loguser['sex'], 0)
-	."\003$c2: \003$c1"
+	.": "
 	.$thread["title"]
-	."\003$c2 (".$fpage.$forum["title"].")"
+	."(".$fpage.$forum["title"].")"
 	." -- "
 	.$link
 	);
